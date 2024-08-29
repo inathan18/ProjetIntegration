e86d6a91a932077babbe0256150af4c1ce063a3a
Nathan Lafreniere-Racine <35408441+inathan18@users.noreply.github.com>
2024-08-29 09:57:05 -0400

    Update Fournisseur.php

--- ProjetPortailFournisseurs/app/Models/Fournisseur.php
+++ ProjetPortailFournisseurs/app/Models/Fournisseur.php
@@ -4,8 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Thiagoprz\EloquentCompositeKey\HasCompositePrimaryKey;
 
 class Fournisseur extends Model
 {
     use HasFactory;
+    use HasCompositePrimaryKey;
+    protected $primaryKey = ['neq', 'email'];
 }
