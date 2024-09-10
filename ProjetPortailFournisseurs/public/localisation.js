3755ec56eeb3d68d15cef70291f3fd9bf0045810
Restyled.io <commits@restyled.io>
2024-09-10 18:03:37 +0000

    Restyled by clang-format

--- ProjetPortailFournisseurs/public/localisation.js
+++ ProjetPortailFournisseurs/public/localisation.js
@@ -1,54 +1,47 @@
-document.addEventListener('DOMContentLoaded', ()=> {
+document.addEventListener('DOMContentLoaded', () => {
+  const selectDrop = document.querySelector('#city');
+  const selectDrop2 = document.querySelector('#province');
 
-    const selectDrop = document.querySelector('#city');
-    const selectDrop2 = document.querySelector('#province');
+  fetch(
+      'https://donneesquebec.ca/recherche/api/action/datastore_search_sql?sql=SELECT%20%22munnom%22%20,%20%22regadm%22%20FROM%20%2219385b4e-5503-4330-9e59-f998f5918363%22')
 
-    
+      .then((response) => {return response.json()
 
-    fetch('https://donneesquebec.ca/recherche/api/action/datastore_search_sql?sql=SELECT%20%22munnom%22%20,%20%22regadm%22%20FROM%20%2219385b4e-5503-4330-9e59-f998f5918363%22')
-  
-    .then((response) => {
-    return response.json()
-   
-    }).then((data) => {
-        let output = ""; 
-        let output2 = ""; 
-        let values = data.result.records ;
+            })
+      .then((data) => {
+        let output = "";
+        let output2 = "";
+        let values = data.result.records;
 
-        let regions = [] ;
+        let regions = [];
 
-        let municipalite = [] ;
+        let municipalite = [];
 
-        // placer les values de la REGADM dans une array pour voir si ils ont déja passé et ensuite les faire sortir une a une grace a une boucle pour les mettre dans un ordre logique pour le choix
+        // placer les values de la REGADM dans une array pour voir si ils ont
+        // déja passé et ensuite les faire sortir une a une grace a une boucle
+        // pour les mettre dans un ordre logique pour le choix
 
         values.forEach(element => {
-            regions.push(element.regadm);
-            municipalite.push(element.munnom);
+          regions.push(element.regadm);
+          municipalite.push(element.munnom);
         })
 
         regions.sort();
-        
-        function suppressionDuplicats(data){
-            return data.filter((value,index) => data.indexOf(value) === index);
+
+        function suppressionDuplicats(data) {
+          return data.filter((value, index) => data.indexOf(value) === index);
         }
 
-        suppressionDuplicats(municipalite).forEach(element => {
-            output += `<option>${element}</option>`;
-        })
+        suppressionDuplicats(municipalite)
+            .forEach(element => { output += `<option>${element}</option>`; })
 
-        suppressionDuplicats(regions).forEach(element => {
-            output2 += `<option>${element}</option>`;
-        })
-        
-
+        suppressionDuplicats(regions).forEach(
+            element => { output2 += `<option>${element}</option>`; })
 
         console.log();
 
         selectDrop.innerHTML = output;
         selectDrop2.innerHTML = output2;
-    }).catch((err) => {
-        console.log(err);
-    })
-
-
+      })
+      .catch((err) => { console.log(err); })
 });
