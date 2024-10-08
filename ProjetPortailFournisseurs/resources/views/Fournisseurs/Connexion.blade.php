db0321d4ff71993bd2d5879ae3cc5506ea9c43ac
Nathan Lafreniere-Racine <35408441+inathan18@users.noreply.github.com>
2024-10-08 13:35:52 -0400

    Update Connexion.blade.php

--- ProjetPortailFournisseurs/resources/views/Fournisseurs/Connexion.blade.php
+++ ProjetPortailFournisseurs/resources/views/Fournisseurs/Connexion.blade.php
@@ -5,8 +5,6 @@
 @section('contenu')
 
 <div class="p-3 text-center"> <h1> Frame Connexion </h1></div>
-    @livewire('recherche-u-n-s-p-s-c')
-    @livewire('validation-r-b-q')
     <form method="post" action="{{route('Fournisseurs.login')}}">
     @csrf
 
