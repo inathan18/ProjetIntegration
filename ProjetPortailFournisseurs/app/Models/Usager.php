13498a38b6c4f2973c1b3cb0a5ce21b4f07e1b93
Nathan Lafreniere-Racine <35408441+inathan18@users.noreply.github.com>
2024-10-29 13:21:54 -0400

    Update Usager.php

--- ProjetPortailFournisseurs/app/Models/Usager.php
+++ ProjetPortailFournisseurs/app/Models/Usager.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Auth\Events\Registered;
 
-class Usager extends Authenticatable
+class Usager extends Authenticatable implements MustVerifyEmail
 {
     use HasFactory, Notifiable;
 
