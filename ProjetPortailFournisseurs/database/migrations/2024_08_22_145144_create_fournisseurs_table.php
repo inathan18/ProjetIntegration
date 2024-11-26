55e605f58ce7e1c17ed3f7648618dfd18dbc5a52
ValentinGir <val.giraudon@gmail.com>
2024-11-26 14:28:47 -0500

    changement statut default

--- ProjetPortailFournisseurs/database/migrations/2024_08_22_145144_create_fournisseurs_table.php
+++ ProjetPortailFournisseurs/database/migrations/2024_08_22_145144_create_fournisseurs_table.php
@@ -25,7 +25,7 @@ return new class extends Migration
             $table->string('website', 255);
             $table->string('email', 100);
             $table->json('files')->nullable();
-            $table->string('statut', 10);
+            $table->string('statut', 10)->default('AT');
             $table->string('neq', 100)->nullable()->unique();
             $table->string('rbq', 100)->nullable();
             $table->json('typesRbq')->nullable();
