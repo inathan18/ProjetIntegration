3d7721ed65af36e6ea0f27879aa47a13c43452e9
ICodder <charliefournier01@hotmail.com>
2024-12-03 18:48:46 -0500

    MAJ Navbar pour la rendre plus viable

--- ProjetPortailFournisseurs/resources/views/partials/navbarUsager.blade.php
+++ ProjetPortailFournisseurs/resources/views/partials/navbarUsager.blade.php
@@ -1,5 +1,7 @@
 <div class="Navbar-Fournisseur paddingNav V3R-GrisFonce">
-  <div class="col-1 text-center V3R-Blanc Standard-logo" style="height:100%;">
-    <a href="{{ route('Fournisseurs.accueil') }}"><img src="{{asset('images/logo.png')}}" width="90" height="90" style="margin-top: 5px;"></a>
+  <div class="col-1 text-center Standard-logo" style="height:100%;">
+    <div class="V3R-Blanc col-10 align-self-center" style="height: 100%;">
+    <a href="{{ route('Fournisseurs.accueil') }}"><img src="{{asset('images/logo.png')}}" width="90" height="90" style="margin-top: 8px; margin-bottom: 8px;"></a>
+    </div>
   </div>
 </div>
