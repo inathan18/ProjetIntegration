5a0a04d06ee205da25f691237e223de73b3850f7
inathan18 <35408441+inathan18@users.noreply.github.com>
2024-12-19 17:34:37 -0500

    AjoutStatutFournisseurSurAccueil

--- ProjetPortailFournisseurs/resources/views/Fournisseurs/Accueil.blade.php
+++ ProjetPortailFournisseurs/resources/views/Fournisseurs/Accueil.blade.php
@@ -68,6 +68,86 @@
                 <div class="col-1"></div>
             </div>
         </div>
+        <div class="col-6">
+            <div class="row">
+                <div class="col-1"></div>
+                <div class="col-10">
+
+                    <div class="card-container">
+                        <div class="persoCard">
+                            <div class="card-top text-center">
+                                <h3> Informations Financières </h3>
+                            </div>
+                            <div class="card-content">
+                                @if($financialInformation->noTps)
+                                    <p>No TPS : 
+                                        <span>{{$financialInformation->noTps}}</span>
+                                    </p>
+                                    <p>No TVQ : 
+                                        <span>{{$financialInformation->noTvq}}</span>
+                                    </p>
+                                    <p>Condition de paiement : 
+                                        <span>{{$financialInformation->conditionPaiement}}</span>
+                                    </p>
+                                    <p>Devise : 
+                                        <span>{{$financialInformation->devise}}</span>
+                                    </p>
+                                    <p>Mode de communication : 
+                                        <span>{{$financialInformation->modeCommunication}}</span>
+                                    </p>
+                                @else
+                                    <p class="text-danger">
+                                        <i class="fas fa-times-circle"></i> Aucune informations financières fournies
+                                    </p>
+                                @endif
+
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col-1"></div>
+
+                <div class="col-1"></div>
+                <div class="col-10">
+
+                    <div class="card-container">
+                        <div class="persoCard">
+                            <div class="card-top text-center">
+                                <h3> Statut </h3>
+                            </div>
+                            <div class="card-content">
+                            <p class="card-text 
+                        @if ($fournisseur_actuel->statut == 'A') text-success
+                        @elseif ($fournisseur_actuel->statut == 'AT') text-warning
+                        @elseif ($fournisseur_actuel->statut == 'AR') text-primary
+                        @elseif ($fournisseur_actuel->statut == 'R') text-danger
+                        @elseif ($fournisseur_actuel->statut == 'D') text-muted
+                        @endif">
+                        
+                        @if ($fournisseur_actuel->statut == 'A')
+                            <i class="fas fa-check-circle"></i> Acceptée
+                        @elseif ($fournisseur_actuel->statut == 'AT')
+                            <i class="fas fa-hourglass-half"></i> En attente
+                        @elseif ($fournisseur_actuel->statut == 'AR')
+                            <i class="fas fa-exclamation-circle"></i> À réviser
+                        @elseif ($fournisseur_actuel->statut == 'R')
+                            <i class="fas fa-times-circle"></i> Refusée
+                        @elseif ($fournisseur_actuel->statut == 'D')
+                            <i class="fas fa-ban"></i> Désactivée
+                        @else
+                            <i class="fas fa-info-circle"></i> Statut inconnu
+                        @endif
+                    </p>
+
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col-1"></div>
+            </div>
+        </div>
 
         <div class="col-6">
             <div class="row">
@@ -109,47 +189,7 @@
             </div>
         </div>
 
-        <div class="col-6">
-            <div class="row">
-                <div class="col-1"></div>
-                <div class="col-10">
-
-                    <div class="card-container">
-                        <div class="persoCard">
-                            <div class="card-top text-center">
-                                <h3> Informations Financières</h3>
-                            </div>
-                            <div class="card-content">
-                                @if($financialInformation->noTps)
-                                    <p>No TPS : 
-                                        <span>{{$financialInformation->noTps}}</span>
-                                    </p>
-                                    <p>No TVQ : 
-                                        <span>{{$financialInformation->noTvq}}</span>
-                                    </p>
-                                    <p>Condition de paiement : 
-                                        <span>{{$financialInformation->conditionPaiement}}</span>
-                                    </p>
-                                    <p>Devise : 
-                                        <span>{{$financialInformation->devise}}</span>
-                                    </p>
-                                    <p>Mode de communication : 
-                                        <span>{{$financialInformation->modeCommunication}}</span>
-                                    </p>
-                                @else
-                                    <p class="text-danger">
-                                        <i class="fas fa-times-circle"></i> Aucune informations financières fournies
-                                    </p>
-                                @endif
-
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-                <div class="col-1"></div>
-            </div>
-        </div>
+       
 
     </div>
 
